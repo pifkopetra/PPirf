854fcad017e2c1d20b68f60c9160a31b58c28927
Petra Pifkó <pifko.petra@gmail.com>
2022-10-07 11:00:50 +0200

    Hetedikfeladat

--- Negyedikhet_T5PM9K/Negyedikhet_T5PM9K/Form1.cs
+++ Negyedikhet_T5PM9K/Negyedikhet_T5PM9K/Form1.cs
@@ -24,6 +24,7 @@ namespace Negyedikhet_T5PM9K
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
@@ -38,7 +39,7 @@ namespace Negyedikhet_T5PM9K
                 xlApp = new Excel.Application();
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
                 xlSheet = xlWB.ActiveSheet;
-                //CreateTable();
+                CreateTable();
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
@@ -53,6 +54,75 @@ namespace Negyedikhet_T5PM9K
 
                 
             }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 1; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i] = headers[i-1];
+            }
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator == true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "=" + GetCell(counter+2,7) + "/" + GetCell(counter+2,6);
+                counter++;
+
+            }
+
+            xlSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+
+            
         }
     }
 }
