684d5d46bcfc1154aabfdc69277facf2617508e9
Petra Pifkó <pifko.petra@gmail.com>
2022-10-20 13:05:56 +0200

    Harmadikfeladat

--- Hatodikhet_T5PM9K/Hatodikhet_T5PM9K/Form1.cs
+++ Hatodikhet_T5PM9K/Hatodikhet_T5PM9K/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hatodikhet_T5PM9K.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,20 @@ namespace Hatodikhet_T5PM9K
         public Form1()
         {
             InitializeComponent();
+            Webszolgaltatashivas();
+        }
+        private void Webszolgaltatashivas()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
