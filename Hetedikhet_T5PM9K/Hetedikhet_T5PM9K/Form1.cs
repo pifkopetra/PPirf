4674958784a301f63f1f32049173315230fd7727
Petra Pifkó <pifko.petra@gmail.com>
2022-11-03 15:32:11 +0100

    Termekvalaszto_gomb

--- Hetedikhet_T5PM9K/Hetedikhet_T5PM9K/Form1.cs
+++ Hetedikhet_T5PM9K/Hetedikhet_T5PM9K/Form1.cs
@@ -14,11 +14,17 @@ namespace Hetedikhet_T5PM9K
     {
         private List<Abstractions.Toy> _toys = new List<Abstractions.Toy>();
 
+        private Abstractions.Toy _nextToy;
+
         private Abstractions.IToyFactory _factory;
         public Abstractions.IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set 
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -53,6 +59,28 @@ namespace Hetedikhet_T5PM9K
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new Entities.CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new Entities.BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+            {
+                Controls.Remove(_nextToy);
+            }
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
