c07a219c544be2a1c33c77c879c25a870a2077d7
Petra Pifkó <pifko.petra@gmail.com>
2022-11-03 14:50:07 +0100

    Auto_letrehozasa

---
+++ Hetedikhet_T5PM9K/Hetedikhet_T5PM9K/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hetedikhet_T5PM9K.Entities
+{
+    public class Car : Abstractions.Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
