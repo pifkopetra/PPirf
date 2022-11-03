6098633ab380955979e985a9d4460f7be92eedea
Petra Pifkó <pifko.petra@gmail.com>
2022-11-03 14:23:26 +0100

    Futoszalag_letrehozasa

--- Hetedikhet_T5PM9K/Hetedikhet_T5PM9K/Form1.cs
+++ Hetedikhet_T5PM9K/Hetedikhet_T5PM9K/Form1.cs
@@ -12,9 +12,47 @@ namespace Hetedikhet_T5PM9K
 {
     public partial class Form1 : Form
     {
+        private List<Entities.Ball> _balls = new List<Entities.Ball>();
+
+        private Entities.BallFactory _factory;
+        public Entities.BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new Entities.BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                {
+                    maxPosition = ball.Left;
+                }
+            }
+
+            if (maxPosition>1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
