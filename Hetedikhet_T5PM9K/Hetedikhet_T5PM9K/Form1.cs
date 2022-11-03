ebbc89f1cd20f006ee52be0ce5d6e1d7ff306907
Petra Pifkó <pifko.petra@gmail.com>
2022-11-03 15:40:13 +0100

    Szinvalaszto_gomb

--- Hetedikhet_T5PM9K/Hetedikhet_T5PM9K/Form1.cs
+++ Hetedikhet_T5PM9K/Hetedikhet_T5PM9K/Form1.cs
@@ -82,5 +82,17 @@ namespace Hetedikhet_T5PM9K
             _nextToy.Left = label1.Left;
             Controls.Add(_nextToy);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
