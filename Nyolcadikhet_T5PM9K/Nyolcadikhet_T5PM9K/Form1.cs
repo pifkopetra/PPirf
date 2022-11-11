0892926d6ffbc69bbc29c6bb706d34cebbebf103
Petra Pifkó <pifko.petra@gmail.com>
2022-11-11 10:12:50 +0100

    Hetedikfeladat

--- Nyolcadikhet_T5PM9K/Nyolcadikhet_T5PM9K/Form1.cs
+++ Nyolcadikhet_T5PM9K/Nyolcadikhet_T5PM9K/Form1.cs
@@ -31,5 +31,19 @@ namespace Nyolcadikhet_T5PM9K
             Portfolio.Add(new Entities.PortfolioItem() { Index = "ELMU", Volume = 10 });
             dataGridView2.DataSource = Portfolio;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                            && date <= x.TradingDay
+                            select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
