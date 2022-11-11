540d2b28d6a9834f19a6aac164899b82f0179a07
Petra Pifkó <pifko.petra@gmail.com>
2022-11-11 09:56:59 +0100

    Negyedikfeladat

--- Nyolcadikhet_T5PM9K/Nyolcadikhet_T5PM9K/Form1.cs
+++ Nyolcadikhet_T5PM9K/Nyolcadikhet_T5PM9K/Form1.cs
@@ -12,9 +12,13 @@ namespace Nyolcadikhet_T5PM9K
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
