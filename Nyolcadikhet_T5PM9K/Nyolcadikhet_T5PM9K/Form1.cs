9d7b9b0a2cccec8b1567ab0bff460e4116b2d005
Petra Pifkó <pifko.petra@gmail.com>
2022-11-11 10:23:54 +0100

    Nyolcadikfeladat

--- Nyolcadikhet_T5PM9K/Nyolcadikhet_T5PM9K/Form1.cs
+++ Nyolcadikhet_T5PM9K/Nyolcadikhet_T5PM9K/Form1.cs
@@ -22,6 +22,24 @@ namespace Nyolcadikhet_T5PM9K
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervallum = 30;
+            DateTime kezdoDatum = (from x in Ticks select x.TradingDay).Min();
+            DateTime zaroDatum = new DateTime(2016, 12, 30);
+            TimeSpan z = zaroDatum - kezdoDatum;
+            for (int i = 0; i < z.Days - intervallum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdoDatum.AddDays(i + intervallum))
+                             - GetPortfolioValue(kezdoDatum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+            var nyeresegekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x).ToList();
+            MessageBox.Show(nyeresegekRendezve[nyeresegekRendezve.Count() / 5].ToString());
+
         }
 
         private void CreatePortfolio()
