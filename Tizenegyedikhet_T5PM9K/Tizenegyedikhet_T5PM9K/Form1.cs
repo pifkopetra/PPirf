5489950bf516a1109427ba607f76c92f61280cc9
Petra Pifkó <pifko.petra@gmail.com>
2022-12-01 10:48:43 +0100

    Hatodikfeladat

--- Tizenegyedikhet_T5PM9K/Tizenegyedikhet_T5PM9K/Form1.cs
+++ Tizenegyedikhet_T5PM9K/Tizenegyedikhet_T5PM9K/Form1.cs
@@ -22,9 +22,26 @@ namespace Tizenegyedikhet_T5PM9K
         public Form1()
         {
             InitializeComponent();
+
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+
+                }
+
+                int nbrOfMales = (from x in Population
+                                 where x.Gender == Gender.Male && x.IsAlive
+                                 select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
