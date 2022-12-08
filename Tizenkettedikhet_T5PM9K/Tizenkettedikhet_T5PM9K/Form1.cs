a02cce31d28803a392a12c8ff0e8b76cb9dd84a6
Petra Pifkó <pifko.petra@gmail.com>
2022-12-08 13:57:45 +0100

    Negyedikfeladat

--- Tizenkettedikhet_T5PM9K/Tizenkettedikhet_T5PM9K/Form1.cs
+++ Tizenkettedikhet_T5PM9K/Tizenkettedikhet_T5PM9K/Form1.cs
@@ -15,11 +15,22 @@ namespace Tizenkettedikhet_T5PM9K
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            //gc.AddPlayer();
+            //gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
